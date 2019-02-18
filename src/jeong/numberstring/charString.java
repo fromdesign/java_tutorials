b22cdc11976a003f124c913a5ea560c25c3523d9
정인엽 <heyjessisexy@naver.com>
2019-02-18 20:23:44 +0900

    complete numberString

---
+++ src/jeong/numberstring/charString.java
@@ -0,0 +1,9 @@
+package jeong.numberstring;
+
+public class charString {
+
+	public static void main(String[] args) {
+		System.out.println("생활코딩입\n니다");
+	}
+
+}
---
+++ src/jeong/numberstring/number.java
@@ -0,0 +1,9 @@
+package jeong.numberstring;
+
+public class number {
+
+	public static void main(String[] args) {
+		System.out.println(1 + 3);
+	}
+
+}
