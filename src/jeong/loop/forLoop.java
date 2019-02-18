1b6f152c441599611c5e6562ff79a4f87595f2c6
정인엽 <heyjessisexy@naver.com>
2019-02-18 20:26:23 +0900

    complete loop

---
+++ src/jeong/loop/forLoop.java
@@ -0,0 +1,14 @@
+package jeong.loop;
+
+public class forLoop {
+
+	public static void main(String[] args) {
+		String name = args[0];
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(name + i);
+		}
+
+	}
+
+}
