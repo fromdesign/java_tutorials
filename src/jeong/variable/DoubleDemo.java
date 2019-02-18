0ba321cb397c644f0dc85b2c10168e573c21646a
정인엽 <heyjessisexy@naver.com>
2019-02-18 20:24:29 +0900

    complete variable

---
+++ src/jeong/variable/DoubleDemo.java
@@ -0,0 +1,13 @@
+package jeong.variable;
+
+public class DoubleDemo {
+
+	public static void main(String[] args) {
+		double a = 1.1;
+		
+		a += 1.1;
+		System.out.println(a);
+
+	}
+
+}
---
+++ src/jeong/variable/intergerDemo.java
@@ -0,0 +1,12 @@
+package jeong.variable;
+
+public class intergerDemo {
+
+	public static void main(String[] args) {
+		int a;
+		a = 1;
+		System.out.println(a + 3);
+
+	}
+
+}
