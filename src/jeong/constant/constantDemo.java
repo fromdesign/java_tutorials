706abae4d18406676bbd3c7636e0416a2bc3c9c7
정인엽 <heyjessisexy@naver.com>
2019-02-21 20:09:19 +0900

    update constant/constantDemo

--- src/jeong/constant/constantDemo.java
+++ src/jeong/constant/constantDemo.java
@@ -3,7 +3,7 @@ package jeong.constant;
 public class constantDemo {
 
 	public static void main(String[] args) {
-		float a = 2.2F;
+		//float a = 2.2F;
 		
 		double b = 2.5F;
 		System.out.println(b);
